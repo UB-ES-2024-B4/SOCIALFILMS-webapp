6c063a6fc9c822c7593fef2e5bf9e6d502b74618
sergi <sergiomarin793@gmail.com>
2024-10-28 14:00:14 +0100

    Fix some errors of QA, others need backend work to resolve

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
 import 'primeicons/primeicons.css'
+import { authErrorCodes } from '~/utils/authErrors'
+import { useToast } from 'primevue/usetoast'
 
 definePageMeta({
   layout: 'empty',
 })
 
 const supabase = useSupabaseClient()
+const toast = useToast()
 
 const credentials = reactive({
   email: '',
   password: '',
-  rememberme: false,
-  failedAuth: false
+  rememberme: false
+})
+
+const credentialsStatus = reactive({
+  failedAuth: false,
+  invalidCredentials: false,
+  emailNotConfirmed: false
 })
 
 const signInLoading = ref(false);
@@ -26,7 +34,18 @@ const handleLogin = async () => {
     if (error) throw error
     navigateTo("/")
   } catch (error) {
-    credentials.failedAuth = true
+    credentialsStatus.failedAuth = true
+
+    switch (error.code) {
+      case authErrorCodes.InvalidCredentials:
+        credentialsStatus.invalidCredentials = true
+        break
+
+      case authErrorCodes.EmailNotConfirmed:
+        toast.add({severity: "warn", summary: 'Email no confirmat', detail:'Si us plau, confirma el teu email per accedir al teu compte.', life: 6500})
+        break
+    }
+
   } finally {
     signInLoading.value = false
   }
@@ -39,8 +58,9 @@ const isNotFilled = computed(() => {
 </script>
 
 <template>
+  <Toast />
   <div class="h-screen w-screen flex items-center justify-center bg-neutral-50 dark:bg-neutral-950">
-    <div class="flex flex-col justify-center p-10 rounded-xl shadow-2xl bg-white w-4/12">
+    <div class="flex flex-col justify-center p-10 rounded-xl shadow-2xl lg:w-4/12 sm:w-7/12 bg-white dark:bg-black dark:shadow-gray-800">
       <h1 class="font-medium text-3xl mb-1">Inicia sessió</h1>
       <h2 class="text-sm mb-5">Hola, benvingut/da de nou👋</h2>
       <form class="flex flex-col gap-y-3" @submit.prevent="handleLogin">
@@ -49,20 +69,26 @@ const isNotFilled = computed(() => {
               <i class="pi pi-envelope"></i>
           </InputGroupAddon>
           <FloatLabel variant="on">
-            <InputText id="on_label_email" v-model="credentials.email" :invalid="credentials.failedAuth"/>
+            <InputText id="on_label_email" v-model="credentials.email" :invalid="credentialsStatus.failedAuth"/>
             <label for="on_label_email">Correu electrònic</label>
           </FloatLabel>
         </InputGroup>
+        
+        <div class="flex flex-col">
+          <InputGroup>
+            <InputGroupAddon>
+                <i class="pi pi-key"></i>
+            </InputGroupAddon>
+            <FloatLabel variant="on">
+              <Password id="on_label_password" v-model="credentials.password" :invalid="credentialsStatus.failedAuth" :feedback="false" toggleMask />
+              <label for="on_label_password">Contrasenya</label>
+            </FloatLabel>
+          </InputGroup>
 
-        <InputGroup>
-          <InputGroupAddon>
-              <i class="pi pi-key"></i>
-          </InputGroupAddon>
-          <FloatLabel variant="on">
-            <Password id="on_label_password" v-model="credentials.password" :invalid="credentials.failedAuth" :feedback="false" toggleMask />
-            <label for="on_label_password">Contrasenya</label>
-          </FloatLabel>
-        </InputGroup>
+          <transition name="p-message" mode="out-in">
+            <Message class="mt-1.5" v-if="credentialsStatus.invalidCredentials" severity="error" icon="pi pi-times-circle">Credencials invàlides</Message>
+          </transition>
+        </div>
 
         <div class="flex items-center justify-between mb-2">
             <div class="flex items-center">
@@ -78,8 +104,8 @@ const isNotFilled = computed(() => {
             <Button type="submit" label="Inicia sessió" :loading="signInLoading" :disabled="isNotFilled" class="flex-grow" />
           </div>
           <div class="flex items-center justify-center">
-            <span class="text-sm text-gray-500">No estàs registrat?</span>
-            <Button as="router-link" size="small" label="Crear un compte" icon="pi pi-arrow-up-right" iconPos="right" to="/signup" target="_blank" link />
+            <span class="text-sm text-gray-500 dark:text-gray-300">No estàs registrat?</span>
+            <Button as="router-link" size="small" label="Crear un compte" icon="pi pi-arrow-up-right" iconPos="right" to="/signup" link />
           </div>
         </div>
       </form>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import 'primeicons/primeicons.css'
+import { authErrorCodes } from '~/utils/authErrors'
 
 definePageMeta({
   layout: 'empty',
@@ -11,17 +12,45 @@ const credentials = reactive({
 	username: '',
   email: '',
 	passwordToConfirm: '',
-  password: '',
-  rememberme: false,
-  failedAuth: false
+  password: ''
+})
+
+const credentialsStatus = reactive({
+  failedAuth: false,
+  emailExists: false,
+  usernameExists: false,
+  emailNotValid: false,
+  passwordWeak: false
 })
 
 const signUpLoading = ref(false)
 const modalInfo = ref(false)
 
 const handleSignUp = async () => {
+  Object.keys(credentialsStatus).forEach(key => {
+    credentialsStatus[key] = false;
+  });
+
   try {
     signUpLoading.value = true
+
+    // const { data: dataUsername } = await supabase
+    //   .from('auth.users')
+    //   .select('id')
+    //   .eq('raw_user_meta_data->>username', credentials.username);
+
+    // const { data: dataEmail } = await supabase
+    //   .from('auth.users')
+    //   .select('id')
+    //   .eq('email', credentials.email);
+    
+    // console.log(dataUsername)
+    // console.log(dataEmail)
+    
+    // if (dataUsername!.length > 0) credentialsStatus.usernameExists = true
+    // if (dataEmail!.length > 0) credentialsStatus.emailExists = true
+    // if (credentialsStatus.emailExists || credentialsStatus.usernameExists) return;
+
     const { data, error } = await supabase.auth.signUp({
       email: credentials.email,
       password: credentials.password,
@@ -34,15 +63,22 @@ const handleSignUp = async () => {
     if (error) throw error
 		modalInfo.value = true
   } catch (error) {
-    credentials.failedAuth = true
+    console.log(error)
+    console.log(error.code)
+
+    credentialsStatus.failedAuth = true
+    switch (error.code) {
+      case authErrorCodes.ValidationFailed:
+        credentialsStatus.emailNotValid = true
+        break
+
+      case authErrorCodes.WeakPassword:
+        credentialsStatus.passwordWeak = true
+        break
+    }
   } finally {
     signUpLoading.value = false
   }
-}
-
-const redirectToLogin = () => {
-  navigateTo('/login');
-  window.close();
 }
 
 const isDifferentPassword = computed(() => {
@@ -62,71 +98,88 @@ const isNotFilled = computed(() => {
 	<Dialog v-model:visible="modalInfo" modal header="Registre completat" :style="{ width: '25rem' }" position="bottom" :draggable="false">
     <div class="flex gap-4 items-start mb-6">
 			<i class="pi pi-exclamation-circle mt-1" style="font-size: 2rem; color: #708090"></i>
-			<span class="text-gray-500 dark:text-surface-400 block">
+			<span class="text-gray-500 dark:text-gray-300 block">
 				Si us plau, revisa el teu email per a confirmar el teu compte i finalitzar el procés de registre.
 			</span>
     </div>
     <div class="flex justify-end gap-2">
-        <Button type="button" label="Acceptar" @click="modalInfo = false; redirectToLogin()"></Button>
+        <Button type="button" label="Acceptar" @click="modalInfo = false; navigateTo('/login')"></Button>
     </div>
 	</Dialog>
   <div class="h-screen w-screen flex items-center justify-center bg-neutral-50 dark:bg-neutral-950">
-    <div class="flex flex-col justify-center p-10 rounded-xl shadow-2xl bg-white w-4/12">
+    <div class="flex flex-col justify-center p-10 rounded-xl shadow-2xl lg:w-4/12 sm:w-7/12 bg-white dark:bg-black dark:shadow-gray-800">
       <h1 class="font-medium text-3xl mb-1">Crear nou compte</h1>
       <h2 class="text-sm mb-5">Hola, benvingut/da👋</h2>
       <form class="flex flex-col gap-y-3" @submit.prevent="handleSignUp">
-        <InputGroup>
-          <InputGroupAddon>
-              <i class="pi pi-user"></i>
-          </InputGroupAddon>
-          <FloatLabel variant="on">
-            <InputText id="on_label_username" v-model="credentials.username" :invalid="credentials.failedAuth"/>
-            <label for="on_label_username">Nom d'usuari</label>
-          </FloatLabel>
-        </InputGroup>
+        <div class="flex flex-col">
+          <InputGroup>
+            <InputGroupAddon>
+                <i class="pi pi-user"></i>
+            </InputGroupAddon>
+            <FloatLabel variant="on">
+              <InputText id="on_label_username" v-model="credentials.username" :invalid="credentialsStatus.failedAuth"/>
+              <label for="on_label_username">Nom d'usuari</label>
+            </FloatLabel>
+          </InputGroup>
+          <transition name="p-message" mode="out-in">
+            <Message class="mt-1.5" v-if="credentialsStatus.usernameExists" severity="error" icon="pi pi-times-circle">L'usuari ya existeix</Message>
+          </transition>
+        </div>
 				
-				<InputGroup>
-          <InputGroupAddon>
-              <i class="pi pi-envelope"></i>
-          </InputGroupAddon>
-          <FloatLabel variant="on">
-            <InputText id="on_label_email" v-model="credentials.email" :invalid="credentials.failedAuth"/>
-            <label for="on_label_email">Correu electrònic</label>
-          </FloatLabel>
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroupAddon>
-              <i class="pi pi-key"></i>
-          </InputGroupAddon>
-          <FloatLabel variant="on">
-						<Password id="on_label_password" v-model="credentials.passwordToConfirm" :invalid="credentials.failedAuth" toggleMask>
-							<template #footer>
-									<Divider />
-									<ul class="pl-2 ml-2 my-0 list-disc leading-normal">
-											<li>Com a mínim una minúscula</li>
-											<li>Com a mínim una majúscula</li>
-											<li>Com a mínim un número</li>
-											<li>Com a mínim un símbol</li>
-											<li>Mínim 6 caràcters</li>
-									</ul>
-							</template>
-						</Password>
-            <label for="on_label_password">Contrasenya</label>
-          </FloatLabel>
-        </InputGroup>
-				<div class="flex flex-col gap-1">
+        <div class="flex flex-col">
+          <InputGroup>
+            <InputGroupAddon>
+                <i class="pi pi-envelope"></i>
+            </InputGroupAddon>
+            <FloatLabel variant="on">
+              <InputText id="on_label_email" v-model="credentials.email" :invalid="credentialsStatus.failedAuth"/>
+              <label for="on_label_email">Correu electrònic</label>
+            </FloatLabel>
+          </InputGroup>
+          <transition name="p-message" mode="out-in">
+            <Message class="mt-1.5" v-if="credentialsStatus.emailExists" severity="error" icon="pi pi-times-circle">El correu electrònic ya existeix</Message>
+          </transition>
+          <transition name="p-message" mode="out-in">
+            <Message class="mt-1.5" v-if="credentialsStatus.emailNotValid" severity="error" icon="pi pi-times-circle">El correu electrònic no és vàlid</Message>
+          </transition>
+        </div>
+				<div class="flex flex-col">
+          <InputGroup>
+            <InputGroupAddon>
+                <i class="pi pi-key"></i>
+            </InputGroupAddon>
+            <FloatLabel variant="on">
+              <Password id="on_label_password" v-model="credentials.passwordToConfirm" :invalid="credentialsStatus.failedAuth" toggleMask>
+                <template #footer>
+                    <Divider />
+                    <ul class="pl-2 ml-2 my-0 list-disc leading-normal">
+                        <li>Com a mínim una minúscula</li>
+                        <li>Com a mínim una majúscula</li>
+                        <li>Com a mínim un número</li>
+                        <li>Com a mínim un símbol</li>
+                        <li>Mínim 6 caràcters</li>
+                    </ul>
+                </template>
+              </Password>
+              <label for="on_label_password">Contrasenya</label>
+            </FloatLabel>
+          </InputGroup>
+          <transition name="p-message" mode="out-in">
+						<Message class="mt-1.5" v-if="credentialsStatus.passwordWeak" severity="error" icon="pi pi-times-circle">La contrasenya no és vàlida</Message>
+					</transition>
+        </div>
+				<div class="flex flex-col">
 					<InputGroup>
 						<InputGroupAddon>
 								<i class="pi pi-key"></i>
 						</InputGroupAddon>
 						<FloatLabel variant="on">
-							<Password id="on_label_confirm_password" v-model="credentials.password" :invalid="isDifferentPassword" :feedback="false" toggleMask />
+							<Password id="on_label_confirm_password" v-model="credentials.password" :invalid="isDifferentPassword || credentialsStatus.failedAuth" :feedback="false" toggleMask />
 							<label for="on_label_confirm_password">Confirmar contrasenya</label>
 						</FloatLabel>
 					</InputGroup>
-					<transition name="p-message" mode="out-in" class="flex flex-col">
-						<Message v-if="isDifferentPassword" closable severity="error" icon="pi pi-times-circle">Les contrasenyes no coinxideixen</Message>
+					<transition name="p-message" mode="out-in">
+						<Message class="mt-1.5" v-if="isDifferentPassword" severity="error" icon="pi pi-times-circle">Les contrasenyes no coinxideixen</Message>
 					</transition>
 				</div>	
 				<Button type="submit" label="Registrar-me" :loading="signUpLoading" :disabled="isNotFilled" class="mt-2"/>
