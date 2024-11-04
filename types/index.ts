d069c1d6aaac41448870bb6c7d87a2b414354e29
sergi <sergiomarin793@gmail.com>
2024-11-04 17:31:36 +0100

    First version of movie page

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,4 @@
-import type { GenreId } from "./genres";
+import type { GenreId } from "./genres"
 
 export interface Film {
   id: number
@@ -23,4 +23,39 @@ export interface Review {
   datetime: Date
   content: string
   rating: number
+}
+
+interface CreditsAPI {
+  id: number
+  cast: CastMember[]
+  crew: CrewMember[]
+}
+
+interface CastMember {
+  adult: boolean
+  gender: number
+  id: number
+  known_for_department: string
+  name: string
+  original_name: string
+  popularity: number
+  profile_path: string | null
+  cast_id: number
+  character: string
+  credit_id: string
+  order: number
+}
+
+interface CrewMember {
+  adult: boolean
+  gender: number
+  id: number
+  known_for_department: string
+  name: string
+  original_name: string
+  popularity: number
+  profile_path: string | null
+  credit_id: string
+  department: string
+  job: string
 }
