4836cf8f2ae63573accebf0c683a5e6be16d689f
sergi <sergiomarin793@gmail.com>
2024-11-06 20:15:48 +0100

    Implement API call

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
+import type { Film, Review } from "~/types";
 import ReviewCard from "~/components/ReviewCard.vue";
 import CreditCard from "~/components/CreditCard.vue";
 
-const backdropOpacity = ref(1)
+const supabase = useSupabaseClient()
+const route = useRoute()
+
+const { data: dataMovie, error: errorMovie } = await supabase.rpc('find_movie_by_id', {movie_id: route.params.id}) as {data: Film[], error: any}
+const { data: dataReviews, error: errorReviews } = await supabase.rpc('get_reviews', {_movie_id: route.params.id}) as {data: Review[], error: any}
+
+const reviews = dataReviews?.map(review => ({
+  ...review,
+  created_at: new Date(review.created_at)
+})) || [];
+
+console.log(dataMovie)
+
 const posterTranslateY = ref(-112) 
 const scrollThreshold = 200
 
 const handleScroll = () => {
   const scrollPosition = window.scrollY
-  backdropOpacity.value = Math.max(0, 1 - scrollPosition / 340)
-  
+
   const newTranslateY = -112 + Math.min(scrollPosition, 100);
   posterTranslateY.value = Math.max(newTranslateY, -112);
 
@@ -28,47 +40,6 @@ onMounted(() => {
 
 onBeforeUnmount(() => {
   window.removeEventListener('scroll', handleScroll)
-})
-
-const fetchFilm = async () => {
-return {
-    title: 'Joker: Folie à Deux',
-    backdrop: 'https://image.tmdb.org/t/p/original/9oYdz5gDoIl8h67e3ccv3OHtmm2.jpg',
-    poster: 'https://image.tmdb.org/t/p/original/lqoMzCcZYEFK729d6qzt349fB4o.jpg',
-    description: 
-    "Más de una década después de los acontecimientos de 'Avatar', los Na'vi Jake Sully, Neytiri y sus hijos viven en paz en los bosques de Pandora hasta que regresan los hombres del cielo. Entonces comienzan los problemas que persiguen sin descanso a la familia Sully, que decide hacer un gran sacrificio para mantener a su pueblo a salvo y seguir ellos con vida.",
-    release_date: '2010-07-16',
-    genres: [18, 80, 53],
-    vote_average: 8.8,
-  };
-};
-
-const fetchReviews = async () => {
-return [
-    { author: 'Alice', content: 'Amazing film with a mind-bending plot!', datetime: new Date("2023-09-27 18:02"), rating: 9 },
-    { author: 'Bob', content: 'Great concept but a bit hard to follow.', datetime: new Date("2024-11-04 12:20"), rating: 7 },
-    // More reviews...
-  ];
-};
-
-// Data bindings
-const film = ref({});
-const reviews = ref([]);
-const genres = { 18: 'Drama', 80: 'Crime', 53: 'Thriller' };
-
-// Computed properties
-const formattedGenres = computed(() => {
-  return film.value.genres ? film.value.genres.map(id => genres[id]).join(' • ') : '';
-});
-
-// Load data on component mount
-onMounted(async () => {
-  film.value = await fetchFilm();
-  reviews.value = await fetchReviews();
-});
-
-onMounted(() => {
-  console.log(window.outerWidth, window.outerHeight);
 })
 
 const visibleDrawerDirector = ref(false)
@@ -99,12 +70,12 @@ const visibleDrawerCast = ref(false)
       rol-or-character="Manolo Aada">
     </CreditCard>
   </Drawer>
-  
+
   <div class="w-full min-h-screen">
     <!-- Backdrop Image -->
     <div
       class="fixed w-full h-full bg-cover bg-center bg-fixed transition-opacity"
-      :style="{ backgroundImage: `url(${film.backdrop})` }"
+      :style="{ backgroundImage: `url(${'https://image.tmdb.org/t/p/original'+dataMovie.backdrop_path})` }"
       >
     </div>
 
@@ -114,39 +85,39 @@ const visibleDrawerCast = ref(false)
         <div class="flex flex-col md:flex-row px-4 pt-4 md:px-10 md:pt-10">
           <!-- Poster -->
           <img
-            :src="film.poster"
-            :alt="`${film.title} poster`"
+            :src="'https://image.tmdb.org/t/p/original'+dataMovie.poster_path"
+            :alt="`${dataMovie.title} poster`"
             class="w-48 md:w-80 rounded-lg shadow-xl md:mr-10 transition-transform duration-500 dark:shadow-gray-300/15"
             :style="{ transform: `translateY(${posterTranslateY}px)` }"
           />
 
           <!-- Film Details -->
           <div class="flex flex-col">
-            <h1 class="text-7xl font-extrabold mb-4">{{ film.title }}</h1>
+            <h1 class="text-7xl font-extrabold mb-4">{{ dataMovie.title }}</h1>
             <div class="flex flex-col flex-1 mt-4 md:flex-row md:mt-0 gap-14">
               <!-- Detalles de la película -->
               <div class="flex-1 flex flex-col space-y-3">
-                <h2 class="text-lg text-gray-600 dark:text-gray-400">{{ formattedGenres }}</h2>
+                <h2 class="text-lg text-gray-600 dark:text-gray-400">{{ dataMovie.genres?.map(genre => genre.name).join(' • ') }}</h2>
               
                 <!-- Ratings and Tags -->
                 <div class="flex items-center space-x-2 mt-4">
-                  <span :class="film.adult ? 
+                  <span :class="dataMovie.adult ? 
                   'tag bg-red-500/20 border border-red-500 text-red-500 dark:border-red-400 dark:text-red-400' :
                   'tag bg-green-500/20 border border-green-500 text-green-500 dark:border-green-400 dark:text-green-400'">
-                    {{ film.adult ? 'R' : 'PG-13' }}
+                    {{ dataMovie.adult ? 'R' : 'PG-13' }}
                   </span>
                   <span class="tag bg-gray-100/50 text-gray-600 dark:bg-gray-700/50 dark:text-gray-200">
                     <i class="pi pi-calendar mr-1.5"></i>
-                    {{ film.release_date }}
+                    {{ dataMovie.release_date }}
                   </span>
                   <span class="tag bg-gray-100/50 text-gray-600 dark:bg-gray-700/50 dark:text-gray-200">
                     <i class="pi pi-star-fill text-yellow-400 mr-1.5"></i> 
-                    {{ film.vote_average }}
+                    {{ dataMovie.vote_average }}
                   </span>
                 </div>
                 <div>
                   <h3 class="text-3xl font-bold mt-4 mb-1.5">Sinopsis</h3>
-                  <p>{{ film.description }}</p>
+                  <p>{{ dataMovie.overview }}</p>
                 </div>
               </div>
 
@@ -193,7 +164,7 @@ const visibleDrawerCast = ref(false)
           <div v-if="reviews.length" class="space-y-4">
             <ReviewCard v-for="(review, index) in reviews" :review="review" :key="index"></ReviewCard>
           </div>
-          <p v-else class="text-gray-400">Encara no hi ha ressenyes.</p>
+          <p v-else class="text-gray-600 dark:text-gray-400">Encara no hi ha ressenyes.</p>
         </div>
       </div>  
     </div>
--- types/index.ts
+++ types/index.ts
@@ -4,13 +4,21 @@ export interface Film {
   id: number
   media_type: string
   poster_path: string
+  backdrop_path: string
   title: string
   overview: string
   adult: boolean
-  genre_ids: GenreId[]
+  genre_ids?: GenreId[]
+  genres?: Genre[]
   release_date: string
+  runtime: number
   vote_average: number
   vote_count: number
+}
+
+interface Genre {
+  id: number
+  name?: string
 }
 
 export interface FilmsAPI {
@@ -19,10 +27,14 @@ export interface FilmsAPI {
 
 export interface Review {
   id: number
-  author: string
-  datetime: Date
-  content: string
+  user_id: number
+  created_at: Date
+  comment: string
+  likes: number
+  dislikes: number
+  shared_count: number
   rating: number
+  editable: boolean
 }
 
 interface CreditsAPI {
