fbbcca0e68190fc2617a8881fe46e9c0fd710efb
Marclosk <marclosquino2@gmail.com>
2024-11-16 11:14:24 +0100

    feat: created tests for get_trending_movies and get_credits_from_movie. Also modifyed the review_add_movie_id_test name so that follows name standards

--- supabase/tests/movie/movie_find_by_name.test.sql
+++ supabase/tests/movie/movie_find_by_name.test.sql
@@ -1,15 +1,15 @@
 BEGIN;
 
--- Planificamos 3 tests
+
 SELECT plan(3);
 
--- Primer test: Verificamos que la función 'search_movie_by_name' no es NULL
+
 SELECT ok(
   (SELECT public.search_movie_by_name('Gladiator') IS NOT NULL), 
   'search_movie_by_name function returns a result for a valid movie name'
 );
 
--- Segundo test: Verificamos que hay más de 0 resultados con el nombre "Gladiator"
+
 SELECT ok(
   (SELECT COUNT(*) > 0
    FROM json_array_elements(public.search_movie_by_name('Gladiator')->'results') AS result
@@ -17,7 +17,7 @@ SELECT ok(
   'search_movie_by_name result contains more than 0 results with the name Gladiator'
 );
 
--- Tercer test: Verificamos que uno de los resultados tiene el id igual a 98
+
 SELECT ok(
   (SELECT EXISTS (
       SELECT 1
