643ca6008ef8dad2d990eec5e0ef617198854adf
ZhehanX <zhehanxiang2002@gmail.com>
2024-11-21 19:10:31 +0100

    add usr_description, usr_birth and usr_gender to profiles table

---
+++ supabase/migrations/20241121180223_add_usr_description_usr_birth_usr_gender_to_profiles_table.sql
@@ -0,0 +1,7 @@
+alter table "public"."profiles" add column "usr_birth" date;
+
+alter table "public"."profiles" add column "usr_description" text;
+
+alter table "public"."profiles" add column "usr_gender" text;
+
+
