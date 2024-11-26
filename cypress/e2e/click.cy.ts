1fe1319255ec0ac42cc36d391fbe96c903ec145d
Marclosk <marclosquino2@gmail.com>
2024-11-26 19:16:36 +0100

    feature: added testing for login and register, added also a migration with a function to delete user by email

--- cypress/e2e/click.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('template spec', () => {
-  it('clicks the link "type"', () => {
-    cy.visit('https://socialfilms.nuxt.dev/')
-    cy.contains('Inicia sessió').click()
-    cy.url().should('include', '/login')
-  })
-})
