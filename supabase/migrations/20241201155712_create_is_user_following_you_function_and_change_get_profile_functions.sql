f3f015db2a0ed4ab83022185d28d617ce4a20ad3
ZhehanX <zhehanxiang2002@gmail.com>
2024-12-01 16:58:52 +0100

    create is_user_following_you function and change get_profile functions to return is_following variable and profile info.

---
+++ supabase/migrations/20241201155712_create_is_user_following_you_function_and_change_get_profile_functions.sql
@@ -0,0 +1,153 @@
+create table "public"."is_following" (
+    "exists" boolean
+);
+
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.is_user_following_you(_follower_username text)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$DECLARE
+  _your_username TEXT;
+BEGIN
+  -- Fetch the current user's username using auth.uid()
+  SELECT username
+  INTO _your_username
+  FROM public.profiles
+  WHERE id = auth.uid();
+
+  -- Check if the follower_username is following _your_username
+  IF EXISTS (
+    SELECT 1
+    FROM public.followers
+    WHERE following_username = _your_username 
+      AND follower_username = _follower_username
+  ) THEN
+    RETURN true;
+  ELSE RETURN false;
+  END if;
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.get_profile_by_id(_user_id uuid)
+ RETURNS json
+ LANGUAGE plpgsql
+AS $function$DECLARE
+  result JSON;
+BEGIN
+  SELECT row_to_json(p)
+  INTO result
+  FROM (
+    SELECT 
+      id,
+      username,
+      email,
+      usr_birth,
+      usr_description,
+      usr_gender,
+      CASE WHEN EXISTS (
+        SELECT 1
+        FROM public.followers
+        WHERE following_id = auth.uid() 
+          AND follower_id = _user_id
+      ) THEN true
+      ELSE false
+      END AS is_following
+    FROM public.profiles
+    WHERE id = _user_id
+  ) p;
+
+  RETURN result;
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.get_profile_by_username(_username text)
+ RETURNS json
+ LANGUAGE plpgsql
+AS $function$DECLARE
+  result JSON;
+  _your_username TEXT;
+BEGIN
+  SELECT username
+  INTO _your_username
+  FROM public.profiles
+  WHERE id = auth.uid();
+
+  SELECT row_to_json(p)
+  INTO result
+  FROM (
+    SELECT 
+      id,
+      username,
+      email,
+      usr_birth,
+      usr_description,
+      usr_gender,
+      CASE WHEN EXISTS (
+        SELECT 1
+        FROM public.followers
+        WHERE following_username = _your_username 
+          AND follower_username = _username
+      ) THEN true
+      ELSE false
+      END AS is_following
+    FROM public.profiles
+    WHERE username = _username
+  ) p;
+
+  RETURN result;
+END;$function$
+;
+
+grant delete on table "public"."is_following" to "anon";
+
+grant insert on table "public"."is_following" to "anon";
+
+grant references on table "public"."is_following" to "anon";
+
+grant select on table "public"."is_following" to "anon";
+
+grant trigger on table "public"."is_following" to "anon";
+
+grant truncate on table "public"."is_following" to "anon";
+
+grant update on table "public"."is_following" to "anon";
+
+grant delete on table "public"."is_following" to "authenticated";
+
+grant insert on table "public"."is_following" to "authenticated";
+
+grant references on table "public"."is_following" to "authenticated";
+
+grant select on table "public"."is_following" to "authenticated";
+
+grant trigger on table "public"."is_following" to "authenticated";
+
+grant truncate on table "public"."is_following" to "authenticated";
+
+grant update on table "public"."is_following" to "authenticated";
+
+grant delete on table "public"."is_following" to "service_role";
+
+grant insert on table "public"."is_following" to "service_role";
+
+grant references on table "public"."is_following" to "service_role";
+
+grant select on table "public"."is_following" to "service_role";
+
+grant trigger on table "public"."is_following" to "service_role";
+
+grant truncate on table "public"."is_following" to "service_role";
+
+grant update on table "public"."is_following" to "service_role";
+
+create policy "Enable read access for all users"
+on "public"."followers"
+as permissive
+for select
+to public
+using (true);
+
+
+
