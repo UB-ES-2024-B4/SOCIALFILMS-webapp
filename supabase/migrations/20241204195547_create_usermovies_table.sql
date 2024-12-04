8a60bac29697b7a84e3cb73bcc5ce79730c2d818
drake-07 <colomedrake@gmail.com>
2024-12-04 20:56:02 +0100

    Tabla User-Movies (guarda las pelis que gustan a los usuarios) creada. Funciones para obtener las peliculas que han gustado a un usuario, obtener usuarios que han dado like a una pelicula, añadir un registro y eliminar un registro

---
+++ supabase/migrations/20241204195547_create_usermovies_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE public."User-Movies" (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY, -- ID único
+    user_id UUID NOT NULL,                        -- ID del usuario
+    movie_id INT NOT NULL,                      -- ID de la pelicula
+
+    -- Restricción: Un usuario no puede dar like a una pelicula mas de una vez
+    CONSTRAINT unique_user_like_movie UNIQUE (user_id, movie_id),
+    FOREIGN KEY (user_id) REFERENCES auth.users(id) ON DELETE CASCADE
+);
+ALTER TABLE public."User-Movies" ENABLE ROW LEVEL SECURITY;
+CREATE POLICY user_access_policy
+ON public."User-Movies"
+FOR ALL
+USING (user_id = auth.uid())
+WITH CHECK (user_id = auth.uid());
+
