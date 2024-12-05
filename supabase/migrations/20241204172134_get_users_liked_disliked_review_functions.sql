ddb5316f273c984e6192fa7c4658cecdea945f09
sergi <sergiomarin793@gmail.com>
2024-12-05 02:53:52 +0100

    Fix remove_reaction function

--- supabase/migrations/20241204172134_get_users_liked_disliked_review_functions.sql
+++ supabase/migrations/20241204172134_get_users_liked_disliked_review_functions.sql
@@ -113,3 +113,37 @@ AS $$
     ORDER BY user_priority DESC, r.created_at DESC
   ) r;
 $$;
+
+CREATE OR REPLACE FUNCTION public.remove_reaction(_review_id UUID)
+RETURNS void
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  current_reaction TEXT;
+BEGIN
+  -- Obtener la reacción actual del usuario
+  SELECT reaction INTO current_reaction
+  FROM public."Review-Reactions"
+  WHERE user_id = auth.uid() AND review_id = _review_id;
+
+  -- Si no hay reacción, salir
+  IF current_reaction IS NULL THEN
+    RETURN;
+  END IF;
+
+   -- Actualizar contadores en Reviews
+  IF current_reaction = 'like' THEN
+    UPDATE public."Reviews"
+    SET likes = likes - 1
+    WHERE id = _review_id;
+  ELSIF current_reaction = 'dislike' THEN
+    UPDATE public."Reviews"
+    SET dislikes = dislikes - 1
+    WHERE id = _review_id;
+  END IF;
+
+  -- Eliminar la reacción
+  DELETE FROM public."Review-Reactions"
+  WHERE user_id = auth.uid() AND review_id = _review_id;
+END;
+$$;
