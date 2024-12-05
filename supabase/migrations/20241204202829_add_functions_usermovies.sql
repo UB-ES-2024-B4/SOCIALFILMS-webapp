ad28c993d5d1d572dd1059060678151b122b571d
sergi <sergiomarin793@gmail.com>
2024-12-05 12:43:36 +0100

    get_user_movies improved for requisists

--- supabase/migrations/20241204202829_add_functions_usermovies.sql
+++ supabase/migrations/20241204202829_add_functions_usermovies.sql
@@ -1,11 +1,33 @@
 --Funcion para obtener las peliculas que les ha gustado a un usuario
-CREATE OR REPLACE FUNCTION get_user_movies()
-RETURNS TABLE(movie_id INT) AS $$
+CREATE OR REPLACE FUNCTION get_user_movies(_relation_type TEXT, _user_id UUID DEFAULT NULL)
+RETURNS json AS $$
+DECLARE
+    movies json;
+    target_user_id UUID := COALESCE(_user_id, auth.uid());
 BEGIN
-    RETURN QUERY
-    SELECT movie_id
+    -- Validar que el tipo de relación no esté vacío
+    IF _relation_type IS NULL OR _relation_type = '' THEN
+        RAISE EXCEPTION 'relation_type no puede ser NULL o vacío';
+    END IF;
+
+    -- Crear una lista de películas con detalles y los indicadores de relación
+    SELECT json_agg(
+        json_build_object(
+            'movie', jsonb_set(
+                find_movie_by_id(movie_id)::jsonb,
+                '{relations}',
+                jsonb_build_object(
+                    'is_favorite', relation_type = 'favorite',
+                    'is_watch_later', relation_type = 'watch_later'
+                )
+            )
+        )
+    )
+    INTO movies
     FROM public."User-Movies"
-    WHERE user_id = auth.uid();
+    WHERE user_id = _user_id AND relation_type = _relation_type;
+
+    RETURN movies;
 END;
 $$ LANGUAGE plpgsql;
 
