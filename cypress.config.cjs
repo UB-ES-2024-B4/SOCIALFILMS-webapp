8058bddf9405f7d44b47e98df5a06094c96a8fca
sergi <sergiomarin793@gmail.com>
2024-12-10 22:25:54 +0100

    delete envConfig on cypress.config

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,8 +1,6 @@
 const { defineConfig } = require("cypress");
-const envConfig = require("./cypress.env.cjs"); // Usar require en lugar de import
 
 module.exports = defineConfig({
-  env: envConfig,
   e2e: {
     setupNodeEvents(on, config) {
       // Implementa eventos de Node si es necesario
