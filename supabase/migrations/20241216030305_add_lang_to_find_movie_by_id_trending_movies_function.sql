c2a017b0fb473a4bb334815540fd7d840f53e1c6
sergi <sergiomarin793@gmail.com>
2024-12-16 03:53:37 +0100

    liltle fix

---
+++ supabase/migrations/20241216030305_add_lang_to_find_movie_by_id_trending_movies_function.sql
@@ -0,0 +1,40 @@
+set check_function_bodies = off;
+
+drop function if exists "public"."find_movie_by_id"(movie_id integer);
+
+CREATE OR REPLACE FUNCTION public.find_movie_by_id(movie_id integer, lang text)
+RETURNS json
+LANGUAGE plpgsql
+AS $function$declare
+  request_url text;
+  api_key text := '32ad583d7cdc0ca52e96bd1db9cb4032';
+  result json;
+BEGIN
+  request_url := format('https://api.themoviedb.org/3/movie/%s?language=%s&api_key=%s', movie_id, lang, api_key);
+  
+  select content::json into result
+  from http_get(request_url);
+
+  return result;
+  
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.get_trending_movies_of_week(lang text)
+ RETURNS json
+ LANGUAGE plpgsql
+AS $function$declare
+  request_url text;
+  api_key text := '32ad583d7cdc0ca52e96bd1db9cb4032';
+  result json;
+BEGIN
+  request_url := format('https://api.themoviedb.org/3/trending/movie/week?language=%s?api_key=%s', lang, api_key);
+  
+  select content::json into result
+  from http_get(request_url);
+
+  return result;
+  
+END;$function$
+;
+
