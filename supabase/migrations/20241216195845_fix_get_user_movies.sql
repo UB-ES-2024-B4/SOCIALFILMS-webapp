348ef5f7804ca672a89dd5951213d4b8280ec8b2
sergi <sergiomarin793@gmail.com>
2024-12-17 00:06:49 +0100

    little fix of migrations

--- supabase/migrations/20241216195845_fix_get_user_movies.sql
+++ supabase/migrations/20241216195845_fix_get_user_movies.sql
@@ -4,29 +4,26 @@ DECLARE
     movies json;
     target_user_id UUID := COALESCE(_user_id, auth.uid());
 BEGIN
-    -- Validar que el tipo de relación no esté vacío
     IF _relation_type IS NULL THEN
         RAISE EXCEPTION 'relation_type no puede ser NULL'
         USING ERRCODE = 'M0001';
     END IF;
 
 
-        -- Crear una lista de películas con detalles y los indicadores de relación
     SELECT json_agg(
-        json_build_object(
-            'movie', jsonb_set(
-                find_movie_by_id(movie_id,'en')::jsonb,
-                '{relations}',
-                jsonb_build_object(
-                    'is_favorite', relation_type = 'favorite',
-                    'is_watch_later', relation_type = 'watch_later'
-                )
+        jsonb_set(
+            find_movie_by_id(movie_id)::jsonb,
+            '{relations}',
+            jsonb_build_object(
+                'is_favorite', relation_type = 'favorite',
+                'is_watch_later', relation_type = 'watch_later'
             )
         )
     )
     INTO movies
     FROM public."User-Movies"
-    WHERE user_id = _user_id AND relation_type = _relation_type;
+    WHERE user_id = target_user_id AND relation_type = _relation_type;
+
     IF movies IS NULL THEN
         RAISE EXCEPTION 'No movies found'
         USING ERRCODE = 'M0002';
