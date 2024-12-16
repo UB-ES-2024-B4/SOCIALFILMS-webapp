260904db0a66498c502ecbda2b04f2f25a255f32
sergi <sergiomarin793@gmail.com>
2024-12-16 03:15:59 +0100

    add request changes

--- supabase/migrations/20241216030305_add_lang_to_find_movie_by_id_function.sql
+++ supabase/migrations/20241216030305_add_lang_to_find_movie_by_id_function.sql
@@ -1,6 +1,8 @@
   set check_function_bodies = off;
 
-  CREATE OR REPLACE FUNCTION public.find_movie_by_id(movie_id integer)
+  drop function if exists "public"."find_movie_by_id"(movie_id integer);
+
+  CREATE OR REPLACE FUNCTION public.find_movie_by_id(movie_id integer, lang text)
   RETURNS json
   LANGUAGE plpgsql
   AS $function$declare
@@ -8,8 +10,8 @@
     api_key text := '32ad583d7cdc0ca52e96bd1db9cb4032';
     result json;
   BEGIN
-    request_url := format('https://api.themoviedb.org/3/movie/%s?api_key=%s', movie_id, api_key);
-
+    request_url := format('https://api.themoviedb.org/3/movie/%s?language=%s&api_key=%s', movie_id, lang, api_key);
+    
     select content::json into result
     from http_get(request_url);
 
