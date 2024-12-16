9e07743aa831421546e1a40f6da1303c1f0e7438
TeiTol <91332345+TeiTol@users.noreply.github.com>
2024-12-16 10:30:57 +0100

    Update [id].vue

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -260,7 +260,8 @@ const handleUserMovieRelation = async (relation_type: 'favorite' | 'watch_later'
     let rpcFunction = "add_user_movie"
     if (relation_type === 'watch_later') {
       rpcFunction = watchLater.value ? "delete_user_movie" : "add_user_movie";
-    } else if (relation_type === 'favorite') {
+    } 
+    if (relation_type === 'favorite') {
       rpcFunction = favorite.value ? "delete_user_movie" : "add_user_movie";
     }
     const { error } = (await supabase.rpc(rpcFunction, {
@@ -270,7 +271,7 @@ const handleUserMovieRelation = async (relation_type: 'favorite' | 'watch_later'
 
     if (error) throw error;
     if (relation_type === 'watch_later') watchLater.value = !watchLater.value;
-    else if (relation_type === 'favorite') favorite.value = !favorite.value;
+    if (relation_type === 'favorite') favorite.value = !favorite.value;
   } catch (e) {
     console.error(`Error handling relation '${relation_type}':`, e);
   }
